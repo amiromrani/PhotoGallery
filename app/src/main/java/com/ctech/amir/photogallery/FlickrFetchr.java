2f0d90b685215da27acd1623c5417a848c8947a8
amiromrani <46926811+amiromrani@users.noreply.github.com>
2019-05-07 08:32:45 -0500

    added json

--- app/src/main/java/com/ctech/amir/photogallery/FlickrFetchr.java
+++ app/src/main/java/com/ctech/amir/photogallery/FlickrFetchr.java
@@ -1,5 +1,8 @@
 package com.ctech.amir.photogallery;
 
+import android.net.Uri;
+import android.util.Log;
+
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -7,6 +10,9 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class FlickrFetchr {
+
+    private static final String TAG = "FlickrFetchr";
+    private static final String API_KEY = "f2dd6b6ce47099ac09227ccdb3fff174";
 
     public byte[] getUrlBytes(String urlSpec) throws IOException {
         URL url = new URL(urlSpec);
@@ -30,6 +36,24 @@ public class FlickrFetchr {
             connection.disconnect();
         }
     }
+
+    public void fetchItems() {
+        try {
+            String url = Uri.parse("https://api.flickr.com/services/rest/")
+                    .buildUpon()
+                    .appendQueryParameter("method", "flickr.photos.getRecent")
+                    .appendQueryParameter("api_key", API_KEY)
+                    .appendQueryParameter("format", "json")
+                    .appendQueryParameter("estras", "url_s")
+                    .build().toString();
+            String jsonString = getUrlString(url);
+            Log.i(TAG, "Received JSON: " + jsonString);
+        }catch (IOException ioe) {
+            Log.e(TAG, "Failed to fetch items", ioe);
+        }
+    }
+
+
     public String getUrlString(String urlSpec) throws IOException {
         return new String(getUrlBytes(urlSpec));
     }
